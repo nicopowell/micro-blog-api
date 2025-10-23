0be600b91791a58c5fa07c45963356178034c8fb
nicopowell <nicopowell30@gmail.com>
2025-10-22 22:33:48 -0300

    se añadio el middleware de cors

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 import express from "express";
-import "dotenv/config"
-import morgan from "morgan"
-import "./src/DB/config.js"
+import "dotenv/config";
+import morgan from "morgan";
+import cors from "cors";
+import "./src/DB/config.js";
 
-import usuariosRouter from "./src/routes/usuarios.routes.js"
-import postsRouter from "./src/routes/post.routes.js"
+import usuariosRouter from "./src/routes/usuarios.routes.js";
+import postsRouter from "./src/routes/post.routes.js";
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 // MIDDLEWARES
-app.use(express.json())
-app.use(morgan("tiny"))
-app.use("/api/usuarios", usuariosRouter)
-app.use('/api/posts', postsRouter)
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors())
 
+// RUTAS
+app.use("/api/usuarios", usuariosRouter);
+app.use("/api/posts", postsRouter);
 
 app.listen(port, () => {
-    console.log("Servidor corriendo en el puerto", port)
-})
+	console.log("Servidor corriendo en el puerto", port);
+});
